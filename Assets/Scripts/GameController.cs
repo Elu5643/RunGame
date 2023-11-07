5685528828eef15e3495aff194a732fd97e5eb79
LAPTOP-5V9RNTCP\ryuse <ryusei200389@icloud.com>
2023-11-07 14:23:48 +0900

    ResultのTextの色を変えた

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Unity.VisualScripting;
+using System.Drawing;
 
 public class GameController : MonoBehaviour
 {
@@ -19,12 +20,14 @@ public class GameController : MonoBehaviour
     // プレイヤー側で死亡した際にこの関数を呼ぶ
     public void FailureGame()
     {
+        resultText.color = new UnityEngine.Color(255, 0, 0, 255);
         StartCoroutine(Result("GameOver"));
     }
 
     // プレイヤー側でクリアした際にこの関数を呼ぶ
     public void ClearGame()
     {
+        resultText.color = new UnityEngine.Color(0, 255, 0, 255);
         StartCoroutine(Result("  Clear "));
     }
 
